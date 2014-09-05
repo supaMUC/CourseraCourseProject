425e5561aaefdb26fb7d7c9120c5f6c720cc07fb
Matthias <matthias.stierle@gmail.com>
2014-09-05 14:51:45 +0200

    App finished

--- server.R
+++ server.R
@@ -1,11 +1,30 @@
 ## Shiny App Server for Coursera Course Project - Developing Data Products
 library(shiny)
-
+library(class)
+library(e1071) 
+classifier <- naiveBayes(Survived ~ .,data=Titanic)
 
 shinyServer(function(input, output) {
   
-  
-  output$distPlot <- renderPlot({
- 
+  output$text <- renderText({
+    ip <- data.frame(Titanic)[NULL,1:3]
+   ip[1,] <- rbind(c(input$class,input$sex,input$age))
+    rs <- as.character(predict(classifier,ip[,]))
+    if (rs == "Yes") {op <-"Looks good, you would have made it!"}
+    if (rs == "No") {op <- "Sorry, but you would have been feeding the dwellers of the sea"}
+  op
   })
+  output$image <- renderImage({
+    ip <- data.frame(Titanic)[NULL,1:3]
+    ip[1,] <- rbind(c(input$class,input$sex,input$age))
+    rs <- as.character(predict(classifier,ip[,]))
+    if (rs == "No") {filename <- normalizePath(file.path('./images',
+                                        'shark.jpeg')) }
+    else {filename <- normalizePath(file.path('./images',
+                                              'boat.gif'))}
+    # Return a list containing the filename and alt text
+    list(src = filename,
+         alt = paste("Image number", input$n))
+    
+  }, deleteFile = FALSE)
 })
--- ui.R
+++ ui.R
@@ -1,16 +1,23 @@
 ## Shiny App UI for Coursera Course Project - Developing Data Products
 
 library(shiny)
-
+dfTitanic <- data.frame(Titanic)
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
   # Application title
   titlePanel("Coursera Course Project - Developing Data Products"),
   
- 
-    mainPanel(
-      plotOutput("distPlot")
-    )
+  sidebarLayout(position = "left",
+                sidebarPanel( selectInput("class","Class:",unique(dfTitanic$Class)),
+                              radioButtons("sex","Sex:",unique(dfTitanic$Sex)),
+                              radioButtons("age","Age:",unique(dfTitanic$Age))),
+                mainPanel(h3("Would you have survived the sinking of the Titanic?")
+                          ,"This little shiny app is making predictions on whether one would have survived 
+                          the sinking of the Titanic or not. The predictor used is Naive Bayes and the basis 
+                          for the analysis is the R example data set 'Titanic'."
+                          ,"From the sidebar on the left you can choose the various input parameters."
+                          ,h4(tableOutput("text"))
+                          ,imageOutput("image"), align = "center")
   )
-)
+))
